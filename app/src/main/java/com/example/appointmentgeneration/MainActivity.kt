0bef7edc496df92a67ba43174966e459152da198
Jeha <yelsum0209@sookmyung.ac.kr>
2024-11-30 16:27:38 +0900

    앱 실행시 연결 페이지 수정

--- app/src/main/java/com/example/appointmentgeneration/MainActivity.kt
+++ app/src/main/java/com/example/appointmentgeneration/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appointmentgeneration
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -20,10 +21,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        if (isUserLoggedIn()) {
+            navController.navigate(R.id.navigation_home)
+        } else {
+            navController.navigate(R.id.loginPage)
+        }
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_mypage, R.id.navigation_home, R.id.navigation_schedules
@@ -31,5 +36,24 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginPage, R.id.signUpFragment -> {
+                    supportActionBar?.hide()
+                    navView.visibility = View.GONE
+                }
+                else -> {
+                    supportActionBar?.show()
+                    navView.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun isUserLoggedIn(): Boolean {
+        // 로그인 상태를 확인하는 로직 구현하기
+        // 로그인 상태라면 true를 반환하고, 그렇지 않으면 false를 반환합니다.
+        return false
     }
 }
