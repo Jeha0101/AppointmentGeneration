57f72999fd7ee34f93612b99e2193e66013cf664
Jeha <yelsum0209@sookmyung.ac.kr>
2024-11-30 16:26:58 +0900

    회원가입 페이지 기능 구현

--- app/src/main/java/com/example/appointmentgeneration/SignUp.kt
+++ app/src/main/java/com/example/appointmentgeneration/SignUp.kt
@@ -1,59 +1,101 @@
 package com.example.appointmentgeneration
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUp.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUp : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var nicknameEditText: EditText
+    private lateinit var idEditText: EditText
+    private lateinit var idCheckIcon: ImageView
+    private lateinit var pwEditText: EditText
+    private lateinit var pwCheckEditText: EditText
+    private lateinit var pwCheckIcon: ImageView
+    private lateinit var signupButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
-    }
+        val rootView = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUp.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        nicknameEditText = rootView.findViewById(R.id.nickname_edit_text)
+        idEditText = rootView.findViewById(R.id.id_edit_text)
+        idCheckIcon = rootView.findViewById(R.id.id_check_icon)
+        pwEditText = rootView.findViewById(R.id.pw_edit_text)
+        pwCheckEditText = rootView.findViewById(R.id.pw_check_edit_text)
+        pwCheckIcon = rootView.findViewById(R.id.pw_check_icon)
+        signupButton = rootView.findViewById(R.id.signup_page_signup_button)
+
+        nicknameEditText.isEnabled = true
+        idEditText.isEnabled = true
+
+        pwEditText.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+        pwCheckEditText.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        // Add listeners
+        idEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (isIdUnique(s.toString())) {
+                    idCheckIcon.setImageResource(R.drawable.check_true)
+                } else {
+                    idCheckIcon.setImageResource(R.drawable.check_grey)
                 }
             }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        pwCheckEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString() == pwEditText.text.toString()) {
+                    pwCheckIcon.setImageResource(R.drawable.check_true)
+                } else {
+                    pwCheckIcon.setImageResource(R.drawable.check_grey)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        signupButton.setOnClickListener {
+            if (areFieldsValid()) {
+                createUser()
+//                findNavController().navigate(R.id.action_signUp_to_nextPage)
+            } else {
+                Toast.makeText(requireContext(), "올바른 값을 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return rootView
+    }
+
+    private fun isIdUnique(id: String): Boolean {
+        // 사용자의 아이디와 중복되는 아이디가 있는지 파이어베이스에서 검사하는 동작을 구현할 것
+        return true
+    }
+
+    private fun areFieldsValid(): Boolean {
+        val nickname = nicknameEditText.text.toString()
+        val id = idEditText.text.toString()
+        val pw = pwEditText.text.toString()
+        val pwCheck = pwCheckEditText.text.toString()
+
+        return nickname.isNotEmpty() &&
+                id.isNotEmpty() &&
+                pw.isNotEmpty() &&
+                pwCheck.isNotEmpty() &&
+                isIdUnique(id) &&
+                pw == pwCheck
+    }
+
+    private fun createUser() {
+        // 사용자를 파이어베이스에 추가하는 코드를 구현할 것
     }
 }
