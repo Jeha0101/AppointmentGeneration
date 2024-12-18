ae4e98c047b50f1991e5ff69f3b1553fe7eba072
scarfaced000 <chaelimm89@gmail.com>
2024-12-18 17:40:09 +0900

    홈 화면 스크롤뷰+이전 오류

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") version "4.4.2"
 }
 
 android {
     namespace = "com.example.appointmentgeneration"
     compileSdk = 34
-    buildFeatures{
-        viewBinding = true
-    }
 
     defaultConfig {
         applicationId = "com.example.appointmentgeneration"
@@ -20,6 +18,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val properties = Properties().apply {
+            load(rootProject.file("local.properties").inputStream())
+        }
+        buildConfigField("String", "NAVER_MAP_CLIENT_ID", "\"${properties.getProperty("NAVER_MAP_CLIENT_ID")}\"")
+        buildConfigField("String", "NAVER_MAP_KEY", "\"${properties.getProperty("NAVER_MAP_KEY")}\"")
     }
 
     buildTypes {
@@ -40,12 +44,23 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
+    // 네이버 맵 SDK
+    //implementation("com.naver.maps:map-sdk:3.15.0")
+    //implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // AndroidX 및 기타 의존성
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -55,11 +70,9 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.firestore.ktx)
+
+    // 테스트 의존성
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/example/appointmentgeneration/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appointmentgeneration/ui/home/HomeFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.appointmentgeneration.BuildConfig
 import com.example.appointmentgeneration.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val naver = BuildConfig.NAVER_MAP_KEY
 
     override fun onCreateView(
         inflater: LayoutInflater,
