f542f4f4b31d3df342478f3efaf1429ad0584eb5
Jeha <yelsum0209@sookmyung.ac.kr>
2024-12-21 20:04:27 +0900

    refactoring ScheduleData.kt

--- app/src/main/java/com/example/appointmentgeneration/ScheduleData.kt
+++ app/src/main/java/com/example/appointmentgeneration/ScheduleData.kt
@@ -4,23 +4,23 @@ import android.os.Parcel
 import android.os.Parcelable
 
 data class ScheduleData(
-    var myLocation: String = "", // 내 위치
-    var friendLocations: MutableList<String> = mutableListOf(), // 친구 위치 (최대 3명)
-    var desiredDestinations: MutableList<String> = mutableListOf(), // 희망 목적지 (최대 10개)
-    var scheduleDate: String? = null, // 약속 날짜
-    var scheduleTime: String? = null, // 약속 시간
-    var budget: Int? = null, // 예산
+    var myLocation: String = "",
+    var friendLocations: MutableList<String> = mutableListOf(),
+    var desiredDestinations: MutableList<String> = mutableListOf(),
+    var scheduleDate: String? = null,
+    var scheduleTime: String? = null,
+    var budget: Int? = null,
     var moods: MutableList<String> = mutableListOf()
 ) : Parcelable {
 
     constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.createStringArrayList() ?: mutableListOf(),
-        parcel.createStringArrayList() ?: mutableListOf(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.createStringArrayList() ?: mutableListOf()
+        myLocation = parcel.readString() ?: "",
+        friendLocations = parcel.createStringArrayList() ?: mutableListOf(),
+        desiredDestinations = parcel.createStringArrayList() ?: mutableListOf(),
+        scheduleDate = parcel.readString(),
+        scheduleTime = parcel.readString(),
+        budget = parcel.readValue(Int::class.java.classLoader) as? Int,
+        moods = parcel.createStringArrayList() ?: mutableListOf()
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -33,17 +33,11 @@ data class ScheduleData(
         parcel.writeStringList(moods)
     }
 
-    override fun describeContents(): Int {
-        return 0
-    }
+    override fun describeContents(): Int = 0
 
     companion object CREATOR : Parcelable.Creator<ScheduleData> {
-        override fun createFromParcel(parcel: Parcel): ScheduleData {
-            return ScheduleData(parcel)
-        }
+        override fun createFromParcel(parcel: Parcel) = ScheduleData(parcel)
 
-        override fun newArray(size: Int): Array<ScheduleData?> {
-            return arrayOfNulls(size)
-        }
+        override fun newArray(size: Int): Array<ScheduleData?> = arrayOfNulls(size)
     }
 }
